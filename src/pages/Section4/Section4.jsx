017cb07c6d6c72b285ae278d683200b7925583be
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-05-27 17:52:23 +0600

    P.0

--- src/pages/Section4/Section4.jsx
+++ src/pages/Section4/Section4.jsx
@@ -7,12 +7,19 @@ const Section4 = () => {
   const parentRef = useRef(null);
 
   return (
-    <section ref={parentRef} id="section4">
+    <>
+      <section ref={parentRef} id="section4">
 
-      <div className="imageWrapper">
+        <div className="imageWrapper">
 
-        <Images />
-        {/* <div className="texts">
+          <Images />
+
+        </div>
+
+
+      </section>
+
+      <div className="texts">
 
         <p>
           Creating meaningful and interactive web experiences is at the heart of what I do. As a frontend developer, I enjoy building visually engaging interfaces with smooth user experiences using modern technologies like React and animation libraries. I care deeply about clean design, performance, and making every interaction feel intuitive and enjoyable.
@@ -24,12 +31,8 @@ const Section4 = () => {
           Outside of coding, I enjoy traveling, exploring new ideas, watching food and travel vlogs, sleeping, and taking time to relax. These moments recharge my creativity and keep me inspired.
         </p>
 
-      </div> */}
-
       </div>
-
-
-    </section>
+    </>
   );
 };
 
